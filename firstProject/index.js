569367bbb02aba3a677f5b901cf2d666c359811d
ipsarioit <rosinskikamil@gmail.com>
2022-03-27 11:53:27 -0700

    na koniec nagrania

--- firstProject/index.js
+++ firstProject/index.js
@@ -1,1 +1,32 @@
+/* 
+author: XYZ
+this program is doring.... */
+
 console.log("It works! Perfect!");
+
+var x = 10;
+let y = "some text";
+
+//function returns answer for all the questions
+function myFirstFunction (){
+
+}
+
+function mySecondFunction ()
+{
+    var a = 0; //this variable must be renamed
+    //var b = "123";
+    var storeNumber;
+
+    console.log(storeNumber);
+
+}
+
+// my first comment
+// my second comment
+
+/*
+very
+long 
+comment
+*/
