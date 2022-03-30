b5c4b9959c40d2d3e256939168f09a63e5b23b53
ipsarioit <rosinskikamil@gmail.com>
2022-03-30 14:45:43 -0700

    arrays final

--- firstProject/index.js
+++ firstProject/index.js
@@ -1,11 +1,35 @@
-let result = true;
-let result2 = false;
+let fruits = ["apple", "banana", "cucumber"];
+fruits.pop();
+fruits.push("pear");
+fruits.shift();
+fruits.unshift("apple");
 
-console.log(result);
-console.log(result2);
+console.log(fruits);
+console.log(fruits.indexOf("bananana"));
+console.log(fruits[2]);
+fruits.splice(1,1);
+console.log(fruits);
 
-let result3 = 1 < 3;
-let result4 = 1 > 3;
+// 1 0 0
+// 0 1 0
+// 0 0 1
 
-console.log(result3);
-console.log(result4);
+let identityMatrix = [[1,0,0],[0,1,0],[0,0,1]];
+
+console.log(identityMatrix[1][2]);
+
+let fruits2 = fruits.slice(0);
+fruits.pop();
+
+console.log(fruits);
+console.log(fruits2);
+
+let fruits3 = ["banana", ...fruits2, "cherry", ...fruits];
+fruits2.pop();
+console.log(fruits3); //spread
+
+let numbers = [4, 5, 7, 2, 1, 9];
+let lowestNumber = Math.min(...numbers);
+console.log(lowestNumber);
+
+console.log(JSON.stringify(fruits3))
