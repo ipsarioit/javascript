871b7d18c166ab0e16ea36455adc1d2dcaa52142
ipsarioit <rosinskikamil@gmail.com>
2022-04-12 12:19:11 -0700

    final

--- firstProject/index.js
+++ firstProject/index.js
@@ -1,37 +1,15 @@
-let a = "Hello!";
-let b = "1";
-let c = 1;
-let d = true;
+let x = 11111;
+let y = null;
 
-let sum1 = b + c; //string
-console.log(sum1);
-console.log(typeof(sum1));
-
-let sum2 = Number(b) + c; 
-console.log(sum2);
-console.log(typeof(sum2));
-
-let sum3 = d + b;
-console.log(sum3);
-console.log(typeof(sum3));
-
-let sum4 = d + c;
-console.log(sum4);
-console.log(typeof(sum4));
-console.log(typeof(d));
-
-console.log("-------");
-console.log(c == b);
-console.log(c === b);
-console.log(c == d);
-console.log(c === d);
-
-console.log("*******");
-console.log(undefined == null);
-console.log(undefined === null);
-
-console.log([] == 0);
-console.log([] === 0);
-
-console.log('' == 0);
-console.log('' === 0);
+if(x == 10){
+    console.log("found 10");
+}
+else if(x == 11){
+    console.log("found 11");
+}
+else if(x == 12){
+    console.log("found 12");
+}
+else{
+    console.log("unknown number");
+}
