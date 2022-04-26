f5f676288f0054cb0fff2942a60db0322d524c04
ipsarioit <rosinskikamil@gmail.com>
2022-04-26 13:26:40 -0700

    fin

--- firstProject/index.js
+++ firstProject/index.js
@@ -1,13 +1,19 @@
 
 try{
-    console.log(X);
+    //console.log(X);
+    //console.log("RUN ANYWAWY!!!");
+    throw new MyException('exception message');
 }
 catch(exception){
     
     if(exception instanceof ReferenceError) console.log("It was small Reference Error:");
+    else if (typeof exception === "string") console.log("It was small triple Error:");
     else console.log("It was different small Error:");
     
     console.log(exception);
+    //console.log("RUN ANYWAWY!!!");
+}
+finally{
+    console.log("RUN ANYWAWY!!!");
 }
 
-console.log("Working fine!");
